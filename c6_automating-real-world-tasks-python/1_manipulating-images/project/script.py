4d466ec087a26efa13930eb82e2b52f888cd48a3
Dhruvesh <dhruveshbaria@gmail.com>
2022-02-05 01:07:23 +0530

    working script

--- c6_automating-real-world-tasks-python/1_manipulating-images/project/script.py
+++ c6_automating-real-world-tasks-python/1_manipulating-images/project/script.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
-
-# Google IT Automation with Python Professional Certificate
-# Elmo Allistair
-# 11 Agu 2020
-
+from os import listdir
 from PIL import Image
-from glob import glob
-import os
 
-# Note: put this script in images folder
-# Iterate through each file in the folder
-for file in glob('ic_*'): # ignore hidden file (images/.DS_Store) from iteration
-    image = Image.open(file).convert('RGB')
-    # print(image.format, image.size, image.mode) # test
-    """
-    For each file:
-    1. Rotate the image 90° clockwise
-    2. Resize the image from 192x192 to 128x128
-    3. Save the image to a new folder in .jpeg format
-    """
-    path, filename = os.path.split(file)
-    filename = os.path.splitext(filename)[0] # get filename without extension
-    image.rotate(270).resize((128,128)).save('/opt/icons/{}.jpeg'.format(filename))
+# set source and target dirs:
+# NOTE: lab calls for new images to be stored to system root
+src_dir = "images/"
+new_dir = "/opt/icons/"
 
-print('OK')
+# set reprocess vars:
+rx_90dg = -90
+rx_size = (128, 128)
+# NOTE: the required output format results in black images because the source
+# TIFF files have transparent backaground which original JPG format doesn't
+# support. PNG would be a more suitable option, but the lab calls for JPEG.
+rx_frmt = "JPEG"
+
+# gather list of image files:
+img_files = [f for f in listdir(src_dir) if f.startswith("ic_")]
+
+# reprocess images:
+for file in img_files:
+    src_img = Image.open(src_dir + file)
+
+    # rotate & resize image:
+    new_img = src_img.rotate(rx_90dg).resize(rx_size)
+
+    # NOTE: we need to convert to RGB here to avoid error:
+    new_img = new_img.convert("RGB")
+
+    # save new output file:
+    new_img.save(new_dir + file, rx_frmt)
